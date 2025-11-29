06223ab1424b9cba4ff23bd79e1e485b608b74e0
Chaosmann09 <56198093+chaosmann09@users.noreply.github.com>
2025-11-29 10:39:39 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+Config.Items = {
+    ["warnweste"] = {
+        name = "Warnweste",
+        clothingComponent = 9,   -- Kleidungskomponenten
+        clothingDrawable = 15,   -- Drawable-Modell
+        clothingTexture = 0      -- Textur
+    },
+    ["pol_warnweste"] = {
+        name = "Polizei Warnweste",
+        clothingComponent = 9,
+        clothingDrawable = 16,
+        clothingTexture = 0
+    },
+    ["kvo_warnweste"] = {
+        name = "KVO Warnweste",
+        clothingComponent = 9,
+        clothingDrawable = 16,
+        clothingTexture = 0
+    },
+    -- Füge hier weitere Items
+}
